f75e2f3aa3eab09d340b9ba470b0eda90eb69f5a
roopesh-jsp <rupzkumar5@gmail.com>
2025-08-22 13:37:14 +0530

    added talents section

---
+++ components/Talents.jsx
@@ -0,0 +1,62 @@
+import { talents } from "@/data/data";
+import Image from "next/image";
+import GradientBackground from "./GradientBox";
+
+import circles from "@/assests/circles.png";
+import triangles from "@/assests/triangles.png";
+import zigzag from "@/assests/zigzag.png";
+import plus from "@/assests/plus.png";
+
+export default function Talents() {
+  return (
+    <section className=" relative py-16 text-center text-white">
+      <h2
+        className="text-4xl md:text-5xl font-extrabold mb-10 
+        text-white
+             "
+      >
+        My <span className="text-indigo-400">Superpowers</span>
+      </h2>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 px-6 md:px-20">
+        {talents.map((talent, index) => (
+          <div key={index} className="p-6 rounded-2xl  transition">
+            <div
+              className={`w-12 h-12 mx-auto mb-4 rounded-full flex bg-opacity-10Pl items-center justify-center ${talent.color}`}
+            >
+              <Image src={talent.icon} width={35} height={35} />
+            </div>
+
+            <h3 className="text-xl font-semibold mb-2">{talent.title}</h3>
+            <p className="text-gray-200 text-sm font-light leading-relaxed">
+              {talent.text}
+            </p>
+          </div>
+        ))}
+      </div>
+      {/* <div className="absolute translate-y-[300px] translate-x-[-150px]">
+        <GradientBackground upside={true} />
+      </div> */}
+      {/* <Image
+        src={circles}
+        alt="circles"
+        className="absolute top-[5%] left-[10%] w-10  opacity-80"
+      />
+      <Image
+        src={triangles}
+        alt="triangles"
+        className="absolute top-[5%] right-[8%] w-12 opacity-80"
+      />
+      <Image
+        src={triangles}
+        alt="zigzag"
+        className="absolute bottom-[5%] left-[6%] w-10 opacity-80"
+      />
+      <Image
+        src={plus}
+        alt="plus"
+        className="absolute bottom-[5%] right-[8%] rotate-12 w-12 opacity-50"
+      /> */}
+    </section>
+  );
+}
---
+++ data/data.js
@@ -0,0 +1,50 @@
+import dev1 from "@/assests/dev1.png";
+import dev2 from "@/assests/dev2.png";
+// import db from "@/assests/db.png";
+import ai from "@/assests/ai.png";
+import tree from "@/assests/tree.png";
+import layers from "@/assests/layers.png";
+
+export const talents = [
+  {
+    icon: dev1,
+    color: "bg-[rgba(84,84,212,0.1)]",
+    title: "Frontend",
+    text: "Skilled in building responsive and interactive user interfaces using React, Next.js, and modern frontend technologies.",
+  },
+  {
+    icon: dev2,
+    title: "Backend",
+    color: "bg-[rgba(254,220,90,0.1)]",
+
+    text: "Experienced in creating scalable and secure RESTful APIs with Node.js, Express, and authentication mechanisms.",
+  },
+  {
+    icon: ai,
+    title: "LangChain",
+    color: "bg-[rgba(254,220,90,0.1)]",
+    text: "Hands-on with building Generative AI applications using LangChain, OpenAI APIs, and prompt engineering.",
+  },
+  {
+    icon: tree,
+    title: "DSA",
+    color: "bg-[rgba(254,220,90,0.1)]",
+
+    text: "Strong grasp of Data Structures and Algorithms with daily problem-solving practice to write optimized solutions.",
+  },
+  {
+    icon: layers,
+    color: "bg-[rgba(240,64,55,0.1)]",
+
+    title: "Core Subjects",
+    text: "Good knowledge of Computer Science fundamentals including OOPS, DBMS, OS, and Computer Networks.",
+  },
+
+  {
+    icon: tree,
+    color: "bg-[rgba(254,220,90,0.1)]",
+
+    title: "Databases",
+    text: "Proficient in working with both SQL and NoSQL databases such as MySQL and MongoDB.",
+  },
+];
